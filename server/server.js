13fe5fe1489d865007c85e5c293e80c8c5a8d8a7
Cody <cody@vieodesign.com>
2018-06-06 17:22:02 -0400

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -42,7 +42,25 @@ app.get('/todos/:id', (req, res) => {
 		if (!todo) {
 			return res.status(404).send();
 		}
-		res.send({todo});
+		res.status(200).send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+app.delete('/todos/:id', (req, res) => {
+	let id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+		res.send(todo);
 	}).catch((e) => {
 		res.status(400).send();
 	});
