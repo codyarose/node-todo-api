18e5b0e256e522d120aa8ce74efa4453885e3dc7
Cody <cody@vieodesign.com>
2018-06-08 14:46:34 -0400

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://cody:codyvieo9@ds251240.mlab.com:51240/node-todo-db' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://cody:codyvieo9@ds251240.mlab.com:51240/node-todo-db');
 
 
 module.exports = { mongoose };
